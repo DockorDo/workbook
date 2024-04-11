39314575f254e10c03105077efc424ee13b13a93
dodo <rguo9659@gmail.com>
2024-04-12 00:53:15 +0800

    feat: promise

--- index.ts
+++ index.ts
@@ -1,15 +1,103 @@
-enum PROMISE_STATE{
-    PENDDING="pending",
-    FULFILLED="fulfilled",
-    REJECTED="rejected"
+enum PROMISE_STATE {
+    PENDDING = "pending",
+    FULFILLED = "fulfilled",
+    REJECTED = "rejected"
 }
 
-class MyPromise{
+type resolve<T> = {
+    (): void
+    (value?: T): void
+}
+type reject<T> = {
+    (): void
+    (reson?: T): void
+}
+type value<T> = null | T
+type reson<T> = null | T
+
+interface onFulfilled<T = any, R = any> {
+    (resolve?: value<T> | null): R
+}
+interface onRejected<T = any, R = any> {
+    (reson?: reson<T>): R
+}
+
+
+class MyPromise<T = undefined>{
+    resolve: resolve<T>
+    reject: reject<T>
     PromiseState: string
-    constructor(){
-        this.PromiseState = PROMISE_STATE.PENDDING
+    value?: value<T>;
+    reson?: reson<T>;
+    onFulfilledCb: onFulfilled[]
+    onRejectedCb: onRejected[]
+
+
+    constructor(executor?: (resolve: resolve<T>, reject: reject<T>) => any) {
+        this.PromiseState = PROMISE_STATE.PENDDING;
+        this.value = null;
+        this.reson = null;
+        this.onFulfilledCb = []
+        this.onRejectedCb = []
+
+        this.reject = (reject?: T) => {
+            if (this.PromiseState !== PROMISE_STATE.PENDDING) return;
+            this.PromiseState = PROMISE_STATE.REJECTED
+
+            this.reson = reject;
+            this.onRejectedCb.forEach(cb => queueMicrotask(cb))
+        }
+        this.resolve = (value?: T) => {
+            if (this.PromiseState !== PROMISE_STATE.PENDDING) return;
+            this.PromiseState = PROMISE_STATE.FULFILLED
+
+            this.value = value
+            this.onFulfilledCb.forEach(cb => queueMicrotask(() => cb()))
+        }
+        try {
+            executor&&executor(this.resolve, this.reject)
+        } catch (err) {
+            this.reject(err as any)
+        }
     }
 
+
+    then<R>(onFulfilled?: onFulfilled<T, R>, onRejected?: onRejected<T, R>): MyPromise<R | undefined> {
+        if (typeof onFulfilled !== 'function' && this.PromiseState === PROMISE_STATE.FULFILLED) return new MyPromise((res) => res())
+        if (typeof onRejected !== 'function' && this.PromiseState === PROMISE_STATE.REJECTED) return new MyPromise((res) => res())
+
+        return new MyPromise((resolve, reject) => {
+            if (this.PromiseState === PROMISE_STATE.FULFILLED && typeof onFulfilled == 'function') {
+                try {
+                    const value = onFulfilled(this.value)
+                    resolve(value)
+                } catch (error) {
+                    reject(error as any)
+                }
+            } else if (this.PromiseState === PROMISE_STATE.REJECTED && typeof onRejected == 'function') {
+                try {
+                    const reson = onRejected(this.reson)
+                    resolve(reson)
+                } catch (error) {
+                    reject(error as any)
+                }
+            } else if (this.PromiseState === PROMISE_STATE.PENDDING) {
+                onFulfilled ?? this.onFulfilledCb.push(onFulfilled!)
+                onRejected ?? this.onRejectedCb.push(onRejected!)
+            }
+        })
+    }
 }
+const p = new MyPromise<number>((res)=>res(1));
+console.log(p)
+p.then(res=>{
+    console.log("test===>",res)
+    return 2
+}).then(res=>{
+    console.log("ok了烙铁",res)
+})
+p.then(res=>{
+    console.log("test===>",res)
+})
 
 export default MyPromise
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,17 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import MyPromise from '..'
 import { vi } from 'vitest'
 
 
 
 test('A promise must be in one of three state:pending,fulfilled or rejected', () => {
-    const myPromise = new MyPromise()
+    const myPromise = new MyPromise(() => void 0)
+    console.log(myPromise.PromiseState)
     expect(["pending", "fulfilled", "rejected"].includes(myPromise.PromiseState)).toBeTruthy()
 })
 
 test("A promise status is established, it cannot be changed", () => {
-    const myPromise1 = new MyPromise((resolve, reject) => {
+    const myPromise1 = new MyPromise<number>((resolve, reject) => {
         resolve();
         reject();
     });
@@ -23,46 +24,48 @@ test("A promise status is established, it cannot be changed", () => {
     expect(myPromise2.PromiseState).toEqual("rejected")
 })
 
-test("promise is an object with a then method", () => {
-    const myPromise = new MyPromise()
+describe("promise is an object with a then method", () => {
+    const myPromise = new MyPromise<number>((res,rej)=>{
+        res(1)
+    })
     expect(myPromise.then).toBeDefined();
 
-    test("the then method accepts two arguments,when status is fulfilled called onFulfilled", () => {
-        const onFulfilled = vi.fn(() => void 0)
-        const onRejected = vi.fn(() => void 0)
-        expect(onFulfilled).toHaveBeenCalledTimes(0);
-        const myPromise = new MyPromise((resolve, reject) => {
-            resolve();
-        })
-        myPromise.then(onFulfilled, onRejected);
-        expect(onFulfilled).toHaveBeenCalled();
-        expect(onFulfilled).toHaveBeenCalledTimes(1);
-        expect(onRejected).toHaveBeenCalledTimes(0);
-    })
-    test("the then method accepts two arguments,when status is rejected called onRejected", () => {
-        const onFulfilled = vi.fn(() => void 0)
-        const onRejected = vi.fn(() => void 0)
-        expect(onRejected).toHaveBeenCalledTimes(0);
-        const myPromise = new MyPromise((resolve, reject) => {
-            reject();
-        })
-        myPromise.then(onFulfilled, onRejected);
-        expect(onRejected).toHaveBeenCalled();
-        expect(onRejected).toHaveBeenCalledTimes(1);
-        expect(onFulfilled).toHaveBeenCalledTimes(0);
-    })
-    test("then may be called multiple times on the same promise",()=>{
-        
+    // test("the then method accepts two arguments,when status is fulfilled called onFulfilled",async () => {
+    //     const onFulfilled = vi.fn(() => void 0)
+    //     const onRejected = vi.fn(() => void 0)
+    //     expect(onFulfilled).toHaveBeenCalledTimes(0);
+    //     const myPromise = new MyPromise((resolve, reject) => {
+    //         resolve();
+    //     })
+    //     await myPromise.then(onFulfilled, onRejected);
+    //     expect(onFulfilled).toHaveBeenCalled();
+    //     expect(onFulfilled).toHaveBeenCalledTimes(1);
+    //     expect(onRejected).toHaveBeenCalledTimes(0);
+    // })
+    // test("the then method accepts two arguments,when status is rejected called onRejected",async () => {
+    //     const onFulfilled = vi.fn(() => void 0)
+    //     const onRejected = vi.fn(() => void 0)
+    //     expect(onRejected).toHaveBeenCalledTimes(0);
+    //     const myPromise = new MyPromise((resolve, reject) => {
+    //         reject();
+    //     })
+    //     await myPromise.then(onFulfilled, onRejected);
+    //     expect(onRejected).toHaveBeenCalled();
+    //     expect(onRejected).toHaveBeenCalledTimes(1);
+    //     expect(onFulfilled).toHaveBeenCalledTimes(0);
+    // })
+    test("then may be called multiple times on the same promise", () => {
+
     })
     test("the then method must return a promise", () => {
-        const myPromise = new MyPromise()
+        const myPromise = new MyPromise<number>((res)=>{res(1)})
         expect(myPromise.then()).toBeInstanceOf(MyPromise)
     })
-    test("then may be called multiple times on the same promise.",async ()=>{
-        const myPromise = new MyPromise((resolve,reject)=>{
+    test("then may be called multiple times on the same promise.", async () => {
+        const myPromise = new MyPromise<number>((resolve, reject) => {
             resolve(1)
         })
-       await expect( myPromise.then((res)=>res+1)).resolves.toEqual(2)
-       await expect( myPromise.then((res)=>res+1)).resolves.toEqual(2)
+        await expect(myPromise.then<number>((res) => res! + 1)).resolves.toEqual(2)
+        await expect(myPromise.then<number>((res) => res! + 1)).resolves.toEqual(2)
     })
 })
